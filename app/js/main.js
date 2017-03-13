954c711172c24e140800ea95e1b48dd9aa24daab
afindon <alison.findon@hitchcock.org>
2017-03-13 14:28:34 -0400

    Initial Commit

---
+++ app/js/main.js
@@ -0,0 +1,15 @@
+// main.js
+$(document).ready(function() {
+  'use strict';
+
+  // comparing with == generates a warning
+  if ('testing' === 'testing') {
+    console.log($);
+  }
+
+});
+
+function add(num1, num2) {
+  'use strict';
+  return num1 * num2;
+}
